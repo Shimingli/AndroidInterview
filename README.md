35bcfe652c2489d2abc78cbe3586a75c5c9e376b
win7 <737141437@qq.com>
2018-08-13 17:07:59 +0800

    1、对数组中只有[0,1,2]的排序，要求时间复杂度为 o(n) ,有点难以理解！两种的方法，一个是折中查找

--- README.md
+++ README.md
@@ -21,5 +21,6 @@
   * 完成 invalidate（）的xmind 流程图  
   * 完成 postInvalidate（） xmind 流程图
   * 完成博客（安卓invalidate()、postInvalidate()、requestLayout()源码分析）https://www.jianshu.com/p/11345ef2b1ef
-
+* 2018.8.13
+  * 对数组中只有[0,1,2]的排序，要求时间复杂度为 o(n) ,有点难以理解！两种的方法，一个是折中查找  
   
--- app/src/main/java/com/android/interview/MainActivity.java
+++ app/src/main/java/com/android/interview/MainActivity.java
@@ -19,6 +19,8 @@ import com.android.interview.listview_demo.ListViewDemoActivity;
 import com.android.interview.merge_and_viewstub_demo.MergePrincipleActivity;
 import com.android.interview.view_source_code.ViewSourceCodeDemoActivity;
 
+import java.util.Arrays;
+
 /**
  * 安卓面试的资料
  */
@@ -136,12 +138,18 @@ public class MainActivity extends AppCompatActivity {
 //        实现一个自定义view，其中含有若干textview，textview文字可换行且自定义- - view的高度可自适应拓展
 
 
-//        编程题：将元素均为0、1、2的数组排序。在手打了一种直接遍历三种数目并打印的方法后让手写实现，手写实现后让再说一种稳定的方法，说了一种通过三个下标遍历一遍实现的方法，读者可自行百度，在此不赘述。
+
+
+
+
+
 //        java内存模型，五个部分，程序计数器、栈、本地栈、堆、方法区。
-
-
 //        每个部分的概念、特点、作用。
+
+        // TODO: 2018/8/13  
 //        类加载的过程，加载、验证、准备、解析、初始化。每个部分详细描述。
+        
+           
 //        加载阶段读入.class文件，class文件时二进制吗，为什么需要使用二进制的方式？
 //        验证过程是防止什么问题？验证过程是怎样的？加载和验证的执行顺序？符号引用的含义？
 //        准备过程的静态成员变量分配空间和设置初始值问题。
@@ -150,6 +158,8 @@ public class MainActivity extends AppCompatActivity {
 //        类卸载的过程及触发条件。
 //        三种类加载器，如何自定义一个类加载器？
 //        双亲委派机制。
+
+
 //        JVM内存分配策略，优先放于eden区、动态对象年龄判断、分配担保策略等。
 //        JVM垃圾回收策略，怎样判对象、类需要被回收？
 //        四种垃圾回收算法标记-清除、复制、标记-整理、分代收集。
@@ -194,6 +204,78 @@ public class MainActivity extends AppCompatActivity {
 //        红黑树的一些特点？怎样保持平衡？
 
 
+
+        //    编程题：将元素均为0、1、2的数组排序。
+        // 时间复杂度：o(n)
+        //两种方法，都是只遍历一次；
+        int[] arr={0,1,2,0,2,1,2,0,1,1,2,0,2,0,2,1,1,0};
+        sortarray1(arr);
+        //arr=[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2]
+        System.out.println("arr="+Arrays.toString(arr));
+        int[] arrTwo={0,1,2,0,2,1,2,0,1,1,2,0,2,0,2,1,1,0};
+        sortarray2(arrTwo);
+        //HelloarrTwo=[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2]
+        System.out.println("arrTwo="+Arrays.toString(arrTwo));
+    }
+
+
+    /*借鉴《算法》中，当数组中存在大量重复元素时，对快排partition方法的改进。
+     *即三个指针，把数组划分成三部分，<x、=x、>x*/
+    public void sortarray2(int[] nums){
+        int a = 0;//指向下一个存放0的位置
+        int b = nums.length-1;//从右向左，下一个存放2的位置
+        int i = a;
+        while(i<=b){
+            if(nums[i] == 1)
+                i++;
+            else if(nums[i] == 2)
+                exchange(nums,i,b--);
+            else if (nums[i]==0) {
+                exchange(nums, i++, a++);
+            }
+        }
+    }
+    /*只能遍历一遍，从左向右遍历*/
+    public void sortarray1(int[] nums){
+        int i=-1;
+        int j=-1;
+        int k=-1;
+        for(int p=0;p<nums.length;p++){
+            if(nums[p] == 0){
+                if(i == -1)
+                    i = 0;//第一个0的位置
+                if(j != -1){
+                    for(int t=p;t>j;t--)
+                        nums[t] = nums[t-1];
+                    nums[j] = 0;
+                    j++;
+                    if(k != -1)k++;
+                }
+                else if(k != -1){
+                    for(int t=p;t>k;t--)
+                        nums[t] = nums[t-1];
+                    nums[k] = 0;
+                    k++;
+                }
+            } else if(nums[p] == 1){
+                if(j == -1){
+                    if(k != -1) j = k;
+                    else j = p;
+                }
+                if(k != -1){
+                    exchange(nums,p,k);
+                    k++;
+                }
+            } else if(nums[p] == 2){
+                if(k == -1)
+                    k = p;
+            }
+        }
+    }
+    public void exchange(int[] nums,int a,int b){
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
     }
 
     //多线程，5个线程内部打印hello和word，hello在前，要求提供一种方法使得5个线程先全部打印出hello后再打印5个word。
